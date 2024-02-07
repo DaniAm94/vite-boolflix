57c0d378a0dc0bca6921a4753c0d40405c6f388c
DaniAm94 <dani.amico94@gmail.com>
2024-02-07 12:57:54 +0100

    first settings

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+
+// Importo l'icona o le icone
+import { faStar as faSolidStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faRegularStar } from '@fortawesome/free-regular-svg-icons';
+
+//Dico alla libreria quali icone caricare
+library.add(faSolidStar, faRegularStar);
+
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('FontAwesomeIcon', FontAwesomeIcon);
+app.mount('#app');
