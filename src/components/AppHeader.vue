90d877ed6ad434eb70193e57bea26f9be68e3923
DaniAm94 <dani.amico94@gmail.com>
2024-02-07 15:16:22 +0100

    added input search bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,8 +3,17 @@ export default {}
 </script>
 
 <template>
-    <header class="bg-primary">
+    <header class="bg-primary d-flex justify-content-between align-items-center">
         <h1>Header</h1>
+
+
+
+        <form>
+            <input type="text" placeholder="Scrivi...">
+            <button class="btn border text-white" type="button">Search</button>
+        </form>
+
+
     </header>
 </template>
 
