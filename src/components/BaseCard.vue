3a00ea16a6bd291610b209a3beb1245b8b5108e4
DaniAm94 <dani.amico94@gmail.com>
2024-02-07 17:51:18 +0100

    updated card components

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -3,17 +3,34 @@ export default {
     name: 'Base Card',
     props: {
         element: Object
+    },
+    methods: {
+        createImagePath(lan) {
+            const url = new URL(`../assets/img/${lan}.png`, import.meta.url);
+            return url.href;
+        }
     }
 }
 </script>
 
 <template>
     <div class="card">
-        <div class="card-info bg-black text-white">
+        <div class="card-info bg-black text-white p-3 ">
             <div>Titolo: {{ element.title }}</div>
             <div>Titolo originale: {{ element.originalTitle }}</div>
-            <div>Lingua: {{ element.language }}</div>
-            <div>Voto: {{ element.vote }}</div>
+
+            <div class="d-flex align-items-center column-gap-3">Lingua:
+                <img class="img-fluid" v-if="element.language === 'en' || element.language === 'it'"
+                    :src="createImagePath(element.language)" :alt="element.language">
+                <span v-else>{{ element.language.toUpperCase() }}</span>
+            </div>
+            <div>Voto: {{ Math.floor(element.vote) }}</div>
         </div>
     </div>
 </template>
+
+<style scoped>
+img {
+    max-height: 20px;
+}
+</style>
--- src/components/DisplayList.vue
+++ src/components/DisplayList.vue
@@ -15,7 +15,7 @@ export default {
 <template>
     <section class="container-fluid">
         <h2>{{ listTitle }}</h2>
-        <div class="row row-cols-5">
+        <div class="row row-cols-5 row-gap-4">
             <div v-for="movie in list" :key="movie.id" class="col">
                 <BaseCard :element="movie" />
             </div>
