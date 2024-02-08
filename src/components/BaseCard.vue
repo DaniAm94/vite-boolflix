81de8fbfa42b1aab81b7d755aed4fc061033aa4b
DaniAm94 <dani.amico94@gmail.com>
2024-02-08 12:16:45 +0100

    implemented star votes

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,4 +1,6 @@
 <script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 export default {
     name: 'Base Card',
     props: {
@@ -7,20 +9,28 @@ export default {
     computed: {
         hasFlag() {
             const flags = ['en', 'it'];
-            return flags.includes(this.element.language)
+            return flags.includes(this.element.language);
         },
         imgPath() {
             const url = new URL(`../assets/img/${this.element.language}.png`, import.meta.url);
             return url.href;
         },
         posterPath() {
-            if (!this.element.poster.includes('null')) return this.element.poster
+            if (!this.element.poster.includes('null'))
+                return this.element.poster;
             else {
                 const url = new URL(`../assets/img/poster-placeholder.png`, import.meta.url);
                 return url.href;
             }
+        },
+        fullStars() {
+            return Math.floor(this.element.vote * 5 / 10);
+        },
+        emptyStars() {
+            return 5 - this.fullStars;
         }
-    }
+    },
+    components: { FontAwesomeIcon }
 }
 </script>
 
@@ -28,14 +38,18 @@ export default {
     <div class="card">
         <img :src="posterPath" :alt="element.name">
         <ul class="card-info bg-black list-unstyled mb-0 ">
-            <li>Titolo: {{ element.title }}</li>
-            <li>Titolo originale: {{ element.originalTitle }}</li>
+            <li><strong>Titolo:</strong> {{ element.title }}</li>
+            <li><strong>Titolo originale:</strong> {{ element.originalTitle }}</li>
 
-            <li class="d-flex align-items-center column-gap-3">Lingua:
+            <li class="d-flex align-items-center column-gap-3"><strong>Lingua:</strong>
                 <img class="img-fluid flag" v-if="hasFlag" :src="imgPath" :alt="element.language">
                 <span v-else>{{ element.language.toUpperCase() }}</span>
             </li>
-            <li>Voto: {{ Math.floor(element.vote) }}</li>
+            <li>
+                <FontAwesomeIcon v-for="star in fullStars" icon="fas fa-star" />
+                <FontAwesomeIcon v-for="star in emptyStars" icon="far fa-star" />
+
+            </li>
         </ul>
     </div>
 </template>
@@ -47,7 +61,8 @@ export default {
     .card-info {
         display: flex;
         flex-direction: column;
-        justify-content: space-between;
+        align-items: center;
+        row-gap: 1rem;
         position: absolute;
         bottom: 0;
         left: 0;
@@ -55,11 +70,17 @@ export default {
         height: 0;
         color: black;
         overflow-y: scroll;
+
         /* Per nascondere la scrollbar */
         -ms-overflow-style: none;
         scrollbar-width: none;
-        transition: height 1s, color 2s;
+        /* -------- */
 
+        transition: height 1s, color 2s, padding 1s;
+
+        strong {
+            color: red;
+        }
     }
 
     &:hover>.card-info {
